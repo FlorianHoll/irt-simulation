ac69dc7d7339e3c3514d136a2e4787c3d6faf182
Florian Holl <florian.holl@waschbaerdatateam.onmicrosoft.com>
2022-11-19 14:04:58 +0100

    Added function to simulate participant responses.
    This simply corresponds to drawing from a binomial distribution
    with size 1 with the probabilities equal to the probabilities
    from the probability matrix which is given by the formula of the model.

--- multidimensional_irt.R
+++ multidimensional_irt.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-#' Multidimensional Item Response Theory probability modeling.
+#' Vectorized multidimensional Item Response Theory probability modeling.
 #'
 #' This function can handle uni- and multidimensional IRT models and 
 #' returns a matrix of probabilities where each entry represents the
@@ -11,8 +11,8 @@ library(dplyr)
 #'
 #' @param alphas The alphas, i.e. the discrimination parameters in the model.
 #'               Matrix of size D x M.
-#' @param betas The betas, i.e. the difficulty parameters of the items.
-#'              Vector of size 1 x M.
+#' @param deltas The deltas, i.e. the easiness parameters of the items.
+#'              Vector of size M x 1.
 #' @param thetas The thetas, i.e. the person abilties. Matrix of size N x D.
 #'
 #' @return A matrix of size N x M representing the probabilities of the persons
@@ -20,7 +20,7 @@ library(dplyr)
 #'
 #' @examples 
 multidimensional_irt <- function(
-    alphas, betas, thetas
+    alphas, deltas, thetas
   ) {
   
   # Shapes
@@ -29,12 +29,33 @@ multidimensional_irt <- function(
   M = ncol(alphas)
   
   # Convert betas to matrix so that matrix addition works
-  betas = betas %>% rep(each=N) %>% matrix(nrow=N)
+  deltas = t(deltas) %>% rep(each=N) %>% matrix(nrow=N)
   
   # IRT formula
-  probability_matrix = 1 / (1 + exp(-(thetas %*% alphas + betas)))
+  probability_matrix = 1 / (1 + exp(-((thetas %*% t(alphas)) + deltas)))
   
   return(probability_matrix)
 }
 
 
+#' Vectorized simulation of IRT responses based on a probability matrix.
+#'
+#' The function will simulate responses of participants given their 
+#' probability to solve the item. Since this probability is simply assumed
+#' to be a draw from a binomial distribution, simulating the responses is
+#' simply the result of a draw from a binomial distribution with size 1.
+#'
+#' @param probability_matrix A matrix of size N x M, where N is the number of
+#'                           participants and M is the number of items.
+#'
+#' @return The simulated responses; a matrix of shape N x M.
+#'
+#' @examples
+simulate_irt_data <- function(probability_matrix) {
+  
+  N = dim(probability_matrix)[1]
+  M = dim(probability_matrix)[2]
+  
+  results <- rbinom(n=N*M, size=1, prob=probability_matrix) %>% matrix(ncol=M)
+  return(results)
+}
