a2b7e6f74ace4484110220b33c2e18e19a85f9b4
Florian Holl <florian.holl@posteo.de>
2022-11-21 11:45:50 +0100

    Added wrapper script to run simulation from command line.

---
+++ simulation.R
@@ -0,0 +1,32 @@
+source("one_simulation_iteration.R")
+
+library(foreach)
+library(yaml)
+library(doParallel)
+
+# Set up parallel backend to use many processors
+cores <- detectCores()
+cl <- makeCluster(cores[1] - 1)
+registerDoParallel(cl)
+
+args <- commandArgs(TRUE)
+filename <- as.double(args[1])
+simulation_name <- as.double(args[2])
+
+params <- yaml::read_yaml(filename)
+parameters <- params[[simulation_name]]  # parameters are stored as a list
+
+final_results <- (
+  foreach::foreach(
+    i = 1:nr_iterations,
+    .packages = c("mirt", "tidyverse")
+  ) %dopar% {
+    do.call(simulate_one_iteration, parameters)
+  }
+)
+stopCluster(cl)
+
+# save simulation results to results folder.
+dir.create("./results")
+filename = paste0("./results/results_", simulation_name, ".Rds")
+saveRDS(final_results, file = filename)
