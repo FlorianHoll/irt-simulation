8255d1bb54b0166ee4adc14d498452d9f8853423
Florian Holl <florian.holl@posteo.de>
2022-11-21 13:16:23 +0100

    Finished wrapper script.

--- simulation.R
+++ simulation.R
@@ -9,18 +9,21 @@ cores <- detectCores()
 cl <- makeCluster(cores[1] - 1)
 registerDoParallel(cl)
 
+# read arguments from the command line
 args <- commandArgs(TRUE)
 filename <- as.double(args[1])
 simulation_name <- as.double(args[2])
 
+# read in the .yml file containing the simulation parameters
 params <- yaml::read_yaml(filename)
 parameters <- params[[simulation_name]]  # parameters are stored as a list
 
+# start the parallelized for loop
 final_results <- (
   foreach::foreach(
     i = 1:nr_iterations,
     .packages = c("mirt", "tidyverse")
-  ) %dopar% {
+  ) %dopar% {  # parallel processing
     do.call(simulate_one_iteration, parameters)
   }
 )
