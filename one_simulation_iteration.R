8f375c619ed38bcf5b4d156d6ac07ff711f10a5f
Florian Holl <florian.holl@posteo.de>
2022-11-21 13:14:32 +0100

    Debugged and finished simulation function.
    The theta parameters were missing values for the G factor, thus leading to
    incompatible matrix shapes when trying to do matrix multiplication. Therefor,e
    an extra dimension for the G factor was added. Furthermore, the results are now
    returned in the form of a list giving back the parameters, the true and estimated
    coefficients as well as their deviation.

--- one_simulation_iteration.R
+++ one_simulation_iteration.R
@@ -10,7 +10,6 @@ source("multidimensional_irt.R")
 #' responses, estimating a model and comparing the estimated coefficients
 #' with the true coefficients.
 #'
-#' @param nr_iterations The number of iterations of the simulation.
 #' @param nr_persons The number of persons in one iteration of the simulation.
 #'                   Defaults to 1000.
 #' @param nr_items The number of items that the simulated test shall have.
@@ -22,19 +21,22 @@ source("multidimensional_irt.R")
 #'         the deviations of the estimated from the true parameters.
 #'
 #' @examples
-simulate_one_iteration <- function(nr_iterations = 1000,
-                                   nr_persons = 1000,
-                                   nr_items = 20,
-                                   nr_dimensions = 3,
-                                   model_type = "2PL") {
+simulate_one_iteration <- function(
+    nr_persons = 1000,
+    nr_items = 20,
+    nr_dimensions = 3,
+    model_type = "2PL"
+  ) {
+
   # Simulate person abilities (draw from normal distribution)
   thetas <- (
-    rnorm(n = nr_persons * nr_dimensions, mean = 0, sd = 2) %>%
-      matrix(nrow = nr_persons)
+    rnorm(n = nr_persons * (nr_dimensions + 1), mean = 0, sd = 2) %>%
+      matrix(nrow = nr_persons, ncol = nr_dimensions + 1)
   )
 
   # Simulate item easiness parameters (draw from normal distribution)
   deltas <- rnorm(n = nr_items, mean = 0, sd = 2)
+  deltas <- -deltas * rgamma(nr_items, shape = 2, rate = 1)
 
   # Simulate item discrimination parameters (draw from gamma distribution)
   factor_loadings <- simulate_factors(nr_items, nr_dimensions)
@@ -47,14 +49,33 @@ simulate_one_iteration <- function(nr_iterations = 1000,
   # Estimate the model.
   model_syntax <- create_model_syntax(nr_items, nr_dimensions, factor_loadings)
   model <- mirt::mirt(
+    data = as.data.frame(simulated_data),
     model = model_syntax,
     itemtype = model_type
   )
 
-  estimated_coefficients <- mirt::coef(model, as.data.frame = T)
-  true_coefficients <- cbind(alphas, deltas)
+  # Calculate differences between the estimated and true values.
+  true_parameters <- cbind(alphas, deltas) %>% as.data.frame()
+  names(true_parameters) <- c(paste0("a", 1:nr_items), "d")
+  estimated_parameters <- (
+    coef(model, simplify = T)$items %>%
+      as.data.frame() %>%
+      select(-g, -u)
+  )
+  deviation <- estimated_parameters - true_parameters
 
-  deviation <- as.data.frame(estimated_coefficients - true_coefficients)
+  # Store results as a list with parameters
+  result <- list(
+    params = list(
+      nr_persons = nr_persons,
+      nr_items = nr_items,
+      nr_dimensions = nr_dimensions,
+      model_type = model_type
+    ),
+    true_parameters = true_parameters,
+    estimated_parameters = estimated_parameters,
+    deviation = deviation
+  )
 
-  return(deviation)
+  return(result)
 }
