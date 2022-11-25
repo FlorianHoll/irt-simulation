96346eb245bbfdc777949b9336664d7660d21b1c
Florian Holl <florian.holl@waschbaerdatateam.onmicrosoft.com>
2022-11-25 13:29:59 +0100

    Deleted old config file; added R script that writes a .yml file with all relevant configs.

--- configs/create_configs.R
+++ configs/create_configs.R
@@ -1,9 +1,10 @@
 library(yaml)
+library(dplyr)
 
 simulations <- expand.grid(
-  nr_items=c(5, 10, 20, 40, 80),
-  nr_dimensions=c(1, 2, 3, 5),
-  nr_persons=c(30, 50, 100, 200, 500, 1000),
+  nr_items=c(5, 10, 20, 40),
+  nr_dimensions=c(1, 2, 3),
+  nr_persons=c(50, 100, 200, 1000),
   model_type="2PL"
 )
 rownames(simulations) <- paste0("simulation", 1:nrow(simulations))
@@ -11,4 +12,10 @@ simulations_list <- simulations %>%
   split(seq(nrow(simulations))) %>% 
   setNames(rownames(simulations))
 
-write_yaml(simulations_list, "./configs/configs.yml")
+simulations_list <- lapply(simulations_list, 
+                           function(x) {
+                             list(params=x, nr_iterations=40)
+                           }
+)
+
+write_yaml(simulations_list, "configs.yml")
